40992878f54cc2a53902111c8aa249c1d8a8d79a
Harshaaliaschinna <harshaaliaschinna@gmail.com>
2015-04-03 11:17:03 +0530

    dbclose.php

---
+++ dbclose.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Author: harshaaliaschinna
+E-mail: harshaaliaschinna@gmail.com
+Script: php-simple-user-login-framework
+*/
+// Closing database connection
+$dbcon=mysqli_close($dbcon);
+?>
