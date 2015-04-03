ca668d032dd755e6b19f811d6dd695e09b3a38e2
Harshaaliaschinna <harshaaliaschinna@gmail.com>
2015-04-03 11:11:41 +0530

    config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Author: harshaaliaschinna
+E-mail: harshaaliaschinna@gmail.com
+Script: php-simple-user-login-framework
+*/
+
+$servername="server_address";  // Server Address
+$serverdb="server_database_name";  // Server Database Name
+$serveruser="server_database_username";  // Server Database Username
+$serverpass="server_pass"; // Server Database Password
+
+
+?>
