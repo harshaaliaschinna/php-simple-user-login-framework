6ea69873f5d225034a247ef8ccca4c12358bc483
Harshaaliaschinna <harshaaliaschinna@gmail.com>
2015-04-03 11:15:49 +0530

    dbopen.php

--- dbopen.php
+++ dbopen.php
@@ -1,11 +1,16 @@
 <?php
+/*
+Author: harshaaliaschinna
+E-mail: harshaaliaschinna@gmail.com
+Script: php-simple-user-login-framework
+*/
 
 include "config.php";
-
+// Creating new connection with MYSQL server
 $dbcon=mysqli_connect($servername,$serveruser,$serverpass,$serverdb);
-
+// If there are problems in connecting to database
 if(!$dbcon)
 {
-    die("Error! establishing Database connection".mysqli_connect_error());
+    die("Error! Establishing Database connection".mysqli_connect_error());
 }
 ?>
