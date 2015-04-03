7cf61b4e66bee30502e4a5090465cb3e67d68a95
Harshaaliaschinna <harshaaliaschinna@gmail.com>
2015-04-03 11:33:25 +0530

    dash.php
    Dashboard

---
+++ dash.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Author: harshaaliaschinna
+E-mail: harshaaliaschinna@gmail.com
+Script: php-simple-user-login-framework
+*/
+// Verifying is user is valid or not
+include "lock.php";
+
+echo "HAI ".$lock_email; 
+
+echo "</br><a href='logout.php'>Logout</a>";
+?>
